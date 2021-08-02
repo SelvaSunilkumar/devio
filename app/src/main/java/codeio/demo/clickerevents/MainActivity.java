53e5219af4d15d5e3fb97cbd2c07516a148038ba
sunilselva3335@gmail.com <sunilselva3335@gmail.com>
2021-08-02 23:28:37 +0530

    Inital Commit

--- app/src/main/java/codeio/demo/clickerevents/MainActivity.java
+++ app/src/main/java/codeio/demo/clickerevents/MainActivity.java
@@ -3,12 +3,31 @@ package codeio.demo.clickerevents;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private LinearLayout clicker;
+    private TextView clickCounter;
+
+    private int counter = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        clicker = findViewById(R.id.clicker);
+        clickCounter = findViewById(R.id.counter);
+
+        clicker.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                counter += 1;
+                clickCounter.setText(String.valueOf(counter));
+            }
+        });
     }
 }
